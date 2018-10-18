e8627be3a1bf7b42e79e36cb676b22f9754bd5db
Alexei Colin <acolin@isi.edu>
2018-10-18 09:43:46 -0400

    mbox client userspace: create dev files in /dev
    DebugFS is not the right place.

--- drivers/soc/hpsc/hpsc-mbox-userspace.c
+++ drivers/soc/hpsc/hpsc-mbox-userspace.c
@@ -1,4 +1,5 @@
-#include <linux/debugfs.h>
+#include <linux/device.h>
+#include <linux/fs.h>
 #include <linux/err.h>
 #include <linux/io.h>
 #include <linux/kernel.h>
@@ -8,6 +9,7 @@
 #include <linux/platform_device.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
+#include <linux/cdev.h>
 
 #define HPSC_MBOX_DATA_REGS     16
 #define MBOX_MAX_MSG_LEN	HPSC_MBOX_DATA_REGS * 4 // each reg is 32-bit
@@ -16,7 +18,13 @@
 #define DT_MBOX_NAMES_PROP  "mbox-names"
 #define DT_MBOXES_CELLS "#mbox-cells"
 
-static struct dentry *root_debugfs_dir;
+#define MBOX_DEVICE_NAME "mbox"
+
+static struct class *class;
+static unsigned major_num = 0;
+static int num_instances = 0;
+
+static struct devf_data *devf_data_ar;
 
 struct mbox_test_device {
 	struct device		*dev;
@@ -25,6 +33,7 @@ struct mbox_test_device {
 
 struct devf_data {
 	struct device		*dev;
+        struct cdev             cdev;
         struct mbox_test_device *tdev;
         struct mbox_client      client;
 	spinlock_t		lock;
@@ -112,10 +121,20 @@ mbox_test_request_channel(struct mbox_client *client, struct device *dev, unsign
 
 static int mbox_test_message_open(struct inode *inodep, struct file *filp)
 {
-        int rc = simple_open(inodep, filp); // doesn't allocate anything
-	struct devf_data *devf_data = filp->private_data;
-	struct mbox_test_device *tdev = devf_data->tdev;
+	struct devf_data *devf_data;
+	struct mbox_test_device *tdev;
 	struct of_phandle_args spec;
+
+        unsigned int major = imajor(inodep);
+        unsigned int minor = iminor(inodep);
+
+        if (major != major_num || minor < 0 || minor >= num_instances)
+                return -ENODEV;
+
+        devf_data = &devf_data_ar[minor];
+        tdev = devf_data->tdev;
+
+        filp->private_data = devf_data;
 
         spin_lock(&devf_data->lock); // protects devf_data->channel pointer
 
@@ -163,7 +182,7 @@ static int mbox_test_message_open(struct inode *inodep, struct file *filp)
                 return -EINVAL;
         }
 
-        return rc;
+        return 0;
 }
 
 static int mbox_test_message_release(struct inode *inodep, struct file *filp)
@@ -279,35 +298,64 @@ out:
 	return ret;
 }
 
-static const struct file_operations mbox_test_message_ops = {
+static const struct file_operations mbox_fops = {
 	.write	= mbox_test_message_write,
 	.read	= mbox_test_message_read,
 	.open	= mbox_test_message_open,
 	.release = mbox_test_message_release,
 };
 
-static int mbox_test_add_debugfs(struct platform_device *pdev,
+static int mbox_device_create(struct devf_data *devf_data,
+                            int major, int minor,
+                            struct class *class,
+                            const char *name)
+{
+        int rc = 0;
+	struct mbox_test_device *tdev = devf_data->tdev;
+        dev_t devno = MKDEV(major, minor);
+        struct device *device;
+
+        cdev_init(&devf_data->cdev, &mbox_fops);
+        devf_data->cdev.owner = THIS_MODULE;
+
+        rc = cdev_add(&devf_data->cdev, devno, 1);
+        if (rc) {
+                dev_err(tdev->dev, "%s: failed to add cdev\n", __func__);
+                return rc;
+        }
+
+        // TODO: double check that device_destroy doesn't need this pointer
+        device = device_create(class, /* parent */ NULL, devno, NULL, name);
+
+        if (IS_ERR(device)) {
+                rc = PTR_ERR(device);
+                dev_err(tdev->dev, "%s: failed to create device\n", __func__);
+                cdev_del(&devf_data->cdev);
+                return rc;
+        }
+        return rc;
+}
+
+static void mbox_device_destroy(struct devf_data *devf_data,
+                            int major, int minor,
+                            struct class *class)
+{
+        device_destroy(class, MKDEV(major, minor));
+        cdev_del(&devf_data->cdev);
+}
+
+static int mbox_create_dev_files(struct platform_device *pdev,
 				 struct mbox_test_device *tdev)
 {
-        struct devf_data *devf_data_ar, *devf_data;
+        struct devf_data *devf_data;
         struct device_node *dt_node = tdev->dev->of_node;
         struct property *names_prop;
         char devf_name[16];
         const char *mbox_name = NULL;
         const char *fname;
-        int num_instances, i;
+        int i;
         int ret, rc;
-
-	if (!debugfs_initialized())
-		return 0;
-
-        num_instances = of_count_phandle_with_args(dt_node, DT_MBOXES_PROP,
-                                                   DT_MBOXES_CELLS);
-        dev_warn(tdev->dev, "%s: num instances in '%s' property: %d\n",
-                 __func__, DT_MBOXES_PROP, num_instances);
-        if (num_instances < 0) {
-		return -EINVAL;
-        }
+        dev_t dev;
 
 	names_prop = of_find_property(dt_node, DT_MBOX_NAMES_PROP, NULL);
         if (names_prop) {
@@ -322,20 +370,30 @@ static int mbox_test_add_debugfs(struct platform_device *pdev,
                       __func__, DT_MBOX_NAMES_PROP);
         }
 
-	root_debugfs_dir = debugfs_create_dir("mailbox", NULL);
-	if (!root_debugfs_dir) {
-		dev_err(&pdev->dev, "Failed to create Mailbox debugfs\n");
-		return -EINVAL;
-	}
-
-
 	devf_data_ar = devm_kzalloc(&pdev->dev, num_instances * sizeof(struct devf_data), GFP_KERNEL);
 	if (devf_data_ar == NULL) {
 		dev_err(&pdev->dev, "failed to alloc mailbox instance state\n");
-		return -ENOMEM;
+		rc = -ENOMEM;
+                goto fail_devf;
+        }
+
+        rc = alloc_chrdev_region(&dev, /* baseminor */ 0, num_instances, MBOX_DEVICE_NAME);
+        if (rc < 0) {
+		dev_err(&pdev->dev, "failed to alloc chrdev region\n");
+		rc = -EFAULT;
+                goto fail_region;
+        }
+        major_num = MAJOR(dev);
+
+        class = class_create(THIS_MODULE, MBOX_DEVICE_NAME);
+        if (IS_ERR(class)) {
+		dev_err(&pdev->dev, "failed to alloc chrdev region\n");
+		rc = -EFAULT;
+                goto fail_class;
         }
 
         for (i = 0; i < num_instances; ++i) {
+
             devf_data = &devf_data_ar[i];
 
             devf_data->dev = &pdev->dev;
@@ -344,8 +402,19 @@ static int mbox_test_add_debugfs(struct platform_device *pdev,
 
 	    spin_lock_init(&devf_data->lock);
 
-            if (mbox_name) { // name from DT node
+            if (names_prop && mbox_name) { // name from DT node
                 fname = mbox_name;
+
+                // Advance the iterator over the names
+                mbox_name = of_prop_next_string(names_prop, mbox_name);
+                if (!mbox_name && i < num_instances - 1) {
+                        dev_err(&pdev->dev,
+                           "fewer items in property '%s' than in property '%s'\n",
+                            DT_MBOX_NAMES_PROP, DT_MBOXES_PROP);
+                        rc = -EFAULT;
+                        goto fail_dev;
+                }
+
             } else { // index with a prefix
                 ret = snprintf(devf_name, sizeof(devf_name), "mbox%u", i);
                 if (ret < 0 || ret >= sizeof(devf_name)) {
@@ -353,33 +422,30 @@ static int mbox_test_add_debugfs(struct platform_device *pdev,
                            "failed to construct mbox device file name: rc %d\n",
                            ret);
                         rc = -EFAULT;
-                        goto fail;
+                        goto fail_dev;
                 }
                 fname = devf_name;
             }
 
-            debugfs_create_file(fname, 0600, root_debugfs_dir,
-                                devf_data, &mbox_test_message_ops);
-
-            if (names_prop) {
-                mbox_name = of_prop_next_string(names_prop, mbox_name);
-                if (!mbox_name && i < num_instances - 1) {
-                        dev_err(&pdev->dev,
-                           "fewer items in property '%s' than in property '%s'\n",
-                            DT_MBOX_NAMES_PROP, DT_MBOXES_PROP);
-                        rc = -EFAULT;
-                        // TODO: cleanup already created debugfs files?
-                        goto fail;
-                }
+            rc = mbox_device_create(devf_data, major_num, i, class, fname);
+            if (rc) {
+		dev_err(&pdev->dev, "failed to construct mailbox device\n");
+                goto fail_dev;
             }
         }
 
         return 0;
-fail:
+fail_dev:
+        for (--i; i <= 0; --i) // i-th has failed, so no cleanup for i-th
+                mbox_device_destroy(&devf_data_ar[i], major_num, i, class);
+        class_destroy(class);
+fail_class:
+        unregister_chrdev_region(MKDEV(major_num, 0), num_instances);
+fail_region:
         kfree(devf_data_ar);
+fail_devf:
 	return rc;
 }
-
 
 static int mbox_test_probe(struct platform_device *pdev)
 {
@@ -399,8 +465,15 @@ static int mbox_test_probe(struct platform_device *pdev)
 
 	spin_lock_init(&tdev->lock);
 
+        num_instances = of_count_phandle_with_args(tdev->dev->of_node,
+                                DT_MBOXES_PROP, DT_MBOXES_CELLS);
+        dev_warn(tdev->dev, "%s: num instances in '%s' property: %d\n",
+                 __func__, DT_MBOXES_PROP, num_instances);
+        if (num_instances < 0) {
+		return -EINVAL;
+        }
 
-	ret = mbox_test_add_debugfs(pdev, tdev);
+	ret = mbox_create_dev_files(pdev, tdev);
 	if (ret)
 		return ret;
 
@@ -410,7 +483,16 @@ static int mbox_test_probe(struct platform_device *pdev)
 
 static int mbox_test_remove(struct platform_device *pdev)
 {
-	debugfs_remove_recursive(root_debugfs_dir);
+        int i;
+
+        for (i = num_instances - 1; i <= 0; --i)
+                mbox_device_destroy(&devf_data_ar[i], major_num, i, class);
+
+        class_destroy(class);
+        unregister_chrdev_region(MKDEV(major_num, 0), num_instances);
+
+        // devf_data deallocated by device manager
+
 	return 0;
 }
 
