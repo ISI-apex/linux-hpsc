f338465a696e43ea66f634f3e2e323f007a21ab1
Connor Imes <cimes@isi.edu>
2018-12-04 14:53:20 -0500

    hpsc-wdt: fixes and add get_timeleft
    We must convert from cycles to seconds.
    Various other fixes and cleanup

--- drivers/watchdog/hpsc-wdt.c
+++ drivers/watchdog/hpsc-wdt.c
@@ -1,36 +1,38 @@
 /*
  * HPSC Chiplet watchdog driver.
  */
-#include <linux/io.h>
-#include <linux/module.h>
-#include <linux/types.h>
-#include <linux/of_address.h>
-#include <linux/watchdog.h>
 #include <linux/clocksource.h>
+#include <linux/cpu.h>
 #include <linux/interrupt.h>
+#include <linux/io.h>
 #include <linux/irq.h>
+#include <linux/module.h>
+#include <linux/of_address.h>
 #include <linux/of_irq.h>
 #include <linux/smp.h>
-#include <linux/cpu.h>
-
+#include <linux/types.h>
+#include <linux/watchdog.h>
 #include "watchdog_pretimeout.h"
 
-#define DEBUG
-
 #define REG__ST1_TERMINAL    0x00
+#define REG__ST1_COUNT       0x08
 #define REG__ST2_TERMINAL    0x10
+#define REG__ST2_COUNT       0x18
 #define REG__CMD_ARM         0x28
 #define REG__CMD_FIRE        0x2c
 #define REG__CONFIG          0x20
-#define REG__STATUS	     0x24
+#define REG__STATUS          0x24
 
 #define REG__CONFIG__EN      0x1
+#define REG__CONFIG__TICKDIV_SHIFT 2
+#define REG__CONFIG__TICKDIV_MASK  0xFF
 #define REG__STATUS__ST1_TIMEOUT 0x1
 
 #define CMD_CLEAR_ARM  0xcd05
 #define CMD_CLEAR_FIRE 0x05cd
 
 #define HPSC_WDT_SIZE 0x10000
+#define HPSC_WDT_CLK_FREQ_HZ 3906250
 
 struct hpsc_wdt { // per cpu
 	struct watchdog_device	wdd;
@@ -49,7 +51,7 @@ struct hpsc_wdt { // per cpu
 //
 // For the same reason (inability to get to the hpsc_wdt_global object from cpu
 // hotplug callbacks), we also don't use platform driver model, which implies
-// all state has to be in a struct hpsc_wdt_globa.  Instead, we use the
+// all state has to be in a struct hpsc_wdt_global.  Instead, we use the
 // TIMER_OF_DECLARE model with only an init function and no cleanup function.
 // An additional advantage of TIMER_OF_DECLARE is that the WDT is initialized
 // much earlier in the init sequence than a platform device would be, so if the
@@ -58,7 +60,7 @@ struct hpsc_wdt { // per cpu
 // cannot be a module, which is similar to drivers for other per-cpu timers.
 static DEFINE_PER_CPU(struct hpsc_wdt, per_cpu_wdt);
 
-// Given the above, there's no point in dynamically allocating a sturct
+// Given the above, there's no point in dynamically allocating a struct
 // hpsc_wdt_global with just this one field; and storing a pointer
 // to this global object in the per cpu state. More efficient and simpler
 // to maintain the global state of the single instance as static variables.
@@ -74,33 +76,67 @@ static irqreturn_t hpsc_wdt_timeout(int irq, void *priv)
 
 	// TODO: unclear if this int flag will be clearable from here or from
 	// EL3 or via CLEAR cmd.
-        status = readl(wdt->regs + REG__STATUS);
+	status = readl(wdt->regs + REG__STATUS);
 	writel(status & ~REG__STATUS__ST1_TIMEOUT, wdt->regs + REG__STATUS);
 
 	watchdog_notify_pretimeout(&wdt->wdd);
 	return IRQ_HANDLED;
 }
 
+// TERMINAL and COUNT registers store time values in nanoseconds, but we report
+// values in seconds and must handle overflow converting to uint.
+static unsigned int hpsc_wdt_sum_sec(struct hpsc_wdt *wdt, size_t off1,
+				     size_t off2)
+{
+	u64 config = readl(wdt->regs + REG__CONFIG);
+	u32 tickdiv = ((config >> REG__CONFIG__TICKDIV_SHIFT) &
+	               REG__CONFIG__TICKDIV_MASK) + 1;
+	u64 sec1 = readq(wdt->regs + off1) / (HPSC_WDT_CLK_FREQ_HZ / tickdiv);
+	u64 sec2 = readq(wdt->regs + off2) / (HPSC_WDT_CLK_FREQ_HZ / tickdiv);
+	if (sec1 > UINT_MAX || sec2 > UINT_MAX || sec1 + sec2 > UINT_MAX)
+		return UINT_MAX;
+	return sec1 + sec2;
+}
+
+static int hpsc_wdt_check_cpu(struct hpsc_wdt *wdt, int cpu, const char *op)
+{
+	if (likely(wdt->cpu == cpu))
+		return 0;
+	pr_err("HPSC WDT: attempted '%s' operation for core %d from core %d\n",
+	       op, wdt->cpu, cpu);
+	return -EINVAL;
+}
+
 static int hpsc_wdt_start(struct watchdog_device *wdog)
 {
-	// Called when userspace opens the device file, so we have to report success.
 	struct hpsc_wdt *wdt = watchdog_get_drvdata(wdog);
 	u32 config;
-
+	int cpu = get_cpu();
+	int ret = hpsc_wdt_check_cpu(wdt, cpu, "start");
+	if (ret)
+		goto out;
 	pr_info("HPSC WDT: cpu %u: start\n", wdt->cpu);
-
 	// TODO: unclear if this will be allowed and if allowed from EL3 only.
 	config = readl(wdt->regs + REG__CONFIG);
 	writel(config | REG__CONFIG__EN, wdt->regs + REG__CONFIG);
-	return 0;
+out:
+	put_cpu();
+	return ret;
 }
 
 static int hpsc_wdt_stop(struct watchdog_device *wdog)
 {
 	// In HPSC WDT HW the monitored target does not have access to disable
 	struct hpsc_wdt *wdt = watchdog_get_drvdata(wdog);
-	pr_info("HPSC WDT: cpu %u: rejected attempt to stop\n", wdt->cpu);
-	return -EINVAL;
+	int cpu = get_cpu();
+	int ret = hpsc_wdt_check_cpu(wdt, cpu, "stop");
+	if (ret)
+		goto out;
+	pr_warn("HPSC WDT: cpu %u: stop not allowed after start\n", wdt->cpu);
+	ret = -EINVAL;
+out:
+	put_cpu();
+	return ret;
 }
 
 static int hpsc_wdt_ping(struct watchdog_device *wdog)
@@ -111,7 +147,7 @@ static int hpsc_wdt_ping(struct watchdog_device *wdog)
 	// we need to either (A) reject the calls from cores that are not the
 	// core associated with this watchdog instance (associated with the
 	// /dev/watchdogN), or (B) allow other any core to kick any other
-	// core's wathcodg (which doesn't sound like good semantics) and
+	// core's watchdog (which doesn't sound like good semantics) and
 	// serialize with a lock here.
 	//
 	// A better design would be (C) if there was only one /dev/watchdog and the
@@ -130,26 +166,35 @@ static int hpsc_wdt_ping(struct watchdog_device *wdog)
 	// In either design, the userspace ought to access the device only from
 	// a pinned process, otherwise the accesses will succeed, but they
 	// won't make much sense, because which timer would be accessed would
-	// be eseentially randomly chosen, as the process migrates (even after
+	// be essentially randomly chosen, as the process migrates (even after
 	// it's already in the kernel space).
 	int cpu = get_cpu();
-	if (cpu != wdt->cpu) {
-		pr_warn("HPSC WDT: attempted to ping from a foreign core\n");
-		put_cpu();
-		return -EINVAL;
-	}
-	pr_info("HPSC WDT: cpu %u: ping\n", cpu);
+	int ret = hpsc_wdt_check_cpu(wdt, cpu, "ping");
+	if (ret)
+		goto out;
+	pr_debug("HPSC WDT: cpu %u: ping\n", cpu);
 	writel(CMD_CLEAR_ARM, wdt->regs + REG__CMD_ARM);
 	writel(CMD_CLEAR_FIRE, wdt->regs + REG__CMD_FIRE);
+out:
 	put_cpu();
-	return 0;
+	return ret;
+}
+
+static unsigned int hpsc_wdt_get_timeleft(struct watchdog_device *wdog)
+{
+	// TODO: are we actually able to read the COUNT registers?
+	struct hpsc_wdt *wdt = watchdog_get_drvdata(wdog);
+	unsigned int timeout = hpsc_wdt_sum_sec(wdt, REG__ST1_TERMINAL,
+						REG__ST2_TERMINAL);
+	unsigned int count = hpsc_wdt_sum_sec(wdt, REG__ST1_COUNT,
+					      REG__ST2_COUNT);
+	return timeout > count ? timeout - count : 0;
 }
 
 static int hpsc_wdt_cpu_up(unsigned int cpu)
 {
-	u32 flags;
+	u32 flags = irq_get_trigger_type(hpsc_wdt_irq);
 	BUG_ON(cpu != smp_processor_id()); // a check on CPU Hotplug API
-	flags = irq_get_trigger_type(hpsc_wdt_irq);
 	pr_info("HPSC WDT: cpu %d up: enable PPI IRQ%d\n", cpu, hpsc_wdt_irq);
 	enable_percpu_irq(hpsc_wdt_irq, flags);
 	return 0;
@@ -168,11 +213,12 @@ static struct watchdog_ops hpsc_wdt_ops = {
 	.start =	hpsc_wdt_start, // method required
 	.stop =		hpsc_wdt_stop,  // method required
 	.ping =		hpsc_wdt_ping,
+	.get_timeleft = hpsc_wdt_get_timeleft,
 };
 
 static struct watchdog_info hpsc_wdt_info = {
-	.options =	WDIOF_SETTIMEOUT | WDIOF_MAGICCLOSE |
-			WDIOF_KEEPALIVEPING | WDIOF_PRETIMEOUT,
+	.options =	WDIOF_KEEPALIVEPING | WDIOC_GETTIMEOUT |
+			WDIOF_PRETIMEOUT | WDIOC_GETTIMELEFT,
 	.identity =	"HPSC Chiplet watchdog timer",
 };
 
@@ -193,8 +239,7 @@ static int hpsc_wdt_percpu_init(struct hpsc_wdt *wdt, unsigned cpu)
 	hpsc_wdt_wdd_init(&wdt->wdd);
 	watchdog_set_drvdata(&wdt->wdd, wdt);
 
-        timeout = readq(wdt->regs + REG__ST1_TERMINAL) +
-		  readq(wdt->regs + REG__ST2_TERMINAL);
+	timeout = hpsc_wdt_sum_sec(wdt, REG__ST1_TERMINAL, REG__ST2_TERMINAL);
 	watchdog_init_timeout(&wdt->wdd, timeout, NULL);
 
 	err = watchdog_register_device(&wdt->wdd);
@@ -209,11 +254,11 @@ static int hpsc_wdt_percpu_init(struct hpsc_wdt *wdt, unsigned cpu)
 
 static int __init hpsc_wdt_init(struct device_node *np)
 {
-	void __iomem *base;
 	struct resource res;
+	void __iomem *base;
 	struct hpsc_wdt *wdt;
 	int cpu;
-	int err, ret;
+	int ret;
 
 	pr_info("HPSC WDT: probe\n");
 
@@ -221,39 +266,37 @@ static int __init hpsc_wdt_init(struct device_node *np)
 		pr_err("HPSC WDT: Failed to get resource from DT node");
 		return -ENODEV;
 	}
-	pr_info("HPSC WDT: res %p %llx\r\n", (void *)res.start, resource_size(&res));
+	pr_debug("HPSC WDT: res %p %llx\n", (void *)res.start,
+		 resource_size(&res));
 	base = ioremap(res.start, resource_size(&res));
 	if (!base) {
 		pr_err("HPSC WDT: Failed to remap watchdog regs");
 		return -ENODEV;
 	}
-	pr_info("HPSC WDT: base %p\r\n", base);
+	pr_debug("HPSC WDT: base %p\n", base);
 
 	for_each_possible_cpu(cpu) {
 		wdt = &per_cpu(per_cpu_wdt, cpu);
 		wdt->cpu = cpu;
 		wdt->regs = base + cpu * HPSC_WDT_SIZE;
-		pr_info("HPSC WDT: cpu %u: regs %p\r\n", cpu, wdt->regs);
-		err = hpsc_wdt_percpu_init(wdt, cpu);
-		if (err) {
-			iounmap(base);
-			return err;
-		}
+		pr_debug("HPSC WDT: cpu %u: regs %p\n", cpu, wdt->regs);
+		ret = hpsc_wdt_percpu_init(wdt, cpu);
+		if (ret)
+			goto init_fail;
 	}
 
 	hpsc_wdt_irq = irq_of_parse_and_map(np, /* idx */ 0);
 	if (!hpsc_wdt_irq) {
 		pr_err("HPSC WDT: Failed to parse/map irq");
-		iounmap(base);
-		return -ENODEV;
+		ret = -ENODEV;
+		goto init_fail;
 	}
 
-	err = request_percpu_irq(hpsc_wdt_irq, hpsc_wdt_timeout,
-			         "hpsc-wdt", &per_cpu_wdt);
-	if (err) {
-		pr_err("HPSC WDT: Failed to register IRQ handler: %d\n", err);
-		iounmap(base);
-		return err;
+	ret = request_percpu_irq(hpsc_wdt_irq, hpsc_wdt_timeout, "hpsc-wdt",
+				 &per_cpu_wdt);
+	if (ret) {
+		pr_err("HPSC WDT: Failed to register IRQ handler: %d\n", ret);
+		goto init_fail;
 	}
 
 	// We have to hook into cpu hotplug events because to enable the
@@ -265,10 +308,13 @@ static int __init hpsc_wdt_init(struct device_node *np)
 		pr_err("HPSC WDT: Failed to register with CPU Hotplug: %d\n", ret);
 		disable_percpu_irq(hpsc_wdt_irq); // for CPU 0 (i.e. ourselves)
 		free_percpu_irq(hpsc_wdt_irq, &per_cpu_wdt);
-		iounmap(base);
-		return ret;
+		goto init_fail;
 	}
 	return 0;
+
+init_fail:
+	iounmap(base);
+	return ret;
 }
 
 TIMER_OF_DECLARE(hpsc_wdt, "hpsc,hpsc-wdt", hpsc_wdt_init);
