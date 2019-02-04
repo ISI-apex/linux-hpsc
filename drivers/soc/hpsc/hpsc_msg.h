0d59b48c68d09e32ba9cf01baa848abc2d25a4e1
Connor Imes <cimes@isi.edu>
2019-02-04 12:36:59 -0500

    hpsc-notif: Add notif-msg layer around hpsc-notif
    Lots of other updates.

--- drivers/soc/hpsc/hpsc_msg.h
+++ drivers/soc/hpsc/hpsc_msg.h
@@ -1,7 +1,11 @@
 #ifndef __HPSC_MSG_H
 #define __HPSC_MSG_H
 
+#include <linux/types.h>
+
 #define HPSC_MSG_SIZE 64
+
+#define HPSC_MSG_DEFINE(name) unsigned char name[HPSC_MSG_SIZE] = {0}
 
 // Message type enumeration
 enum hpsc_msg_type {
@@ -28,5 +32,21 @@ enum hpsc_msg_type {
 	// enum counter
 	HPSC_MSG_TYPE_COUNT
 };
+
+/**
+ * Send a message that a watchdog timed out.
+ *
+ * @param cpu The CPU whose watchdog timed out
+ */
+void hpsc_msg_wdt_timeout(unsigned int cpu);
+
+/**
+ * Process a received messaged. Should only be called by hpsc-notif.
+ *
+ * @param msg The received message
+ * @param sz The size of the message
+ * @return 0 on success, a negative error code otherwise
+ */
+int hpsc_msg_process(const void *msg, size_t sz);
 
 #endif /* __HPSC_MSG_H */
