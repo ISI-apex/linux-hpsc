bed27820b0a1337c6d4ce1fa97b3629d1bb228b7
Alexei Colin <acolin@isi.edu>
2019-11-06 10:03:12 -0500

    soc: hpsc: msg-tp-shmem: support regions in RAM
    Region outside of RAM are memmap/iomap'ed. Regions within RAM
    cannot be memmap/iomap'ed, they are vmap'ed. Most of the time
    regions will be in RAM, but maintain support for regions outside
    of RAM (e.g. in an offchip memory).
    
    Implemented in the same way as pstore ramoops buffer.

--- drivers/soc/hpsc/hpsc-msg-tp-shmem.c
+++ drivers/soc/hpsc/hpsc-msg-tp-shmem.c
@@ -10,11 +10,13 @@
 #include <linux/io.h>
 #include <linux/kernel.h>
 #include <linux/kthread.h>
+#include <linux/mm.h>
 #include <linux/module.h>
 #include <linux/notifier.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
 #include <linux/platform_device.h>
+#include <linux/slab.h>
 #include <linux/spinlock.h>
 #include "hpsc_msg.h"
 #include "hpsc_notif.h"
@@ -27,11 +29,17 @@ struct hpsc_shmem_region {
 	u32 status;
 };
 
+enum direction_mask {
+	IN  = 0x1,
+	OUT = 0x2,
+};
+
 struct hpsc_msg_tp_shmem_dev {
 	struct device			*dev;
 	spinlock_t			lock;
 	struct hpsc_shmem_region	*in;
 	struct hpsc_shmem_region	*out;
+	enum direction_mask		is_ram;
 	struct notifier_block		nb;
 	struct task_struct		*t;
 	unsigned int			poll_interval_ms;
@@ -77,9 +85,38 @@ static int hpsc_msg_tp_shmem_recv(void *arg)
 	return 0;
 }
 
-static int hpsc_msg_tp_shmem_parse_dt_mreg(struct hpsc_msg_tp_shmem_dev *tdev,
-				     const char *name,
-				     struct hpsc_shmem_region **reg)
+static void *hpsc_msg_tp_shmem_vmap(phys_addr_t start, size_t size)
+{
+	struct page **pages;
+	phys_addr_t page_start;
+	unsigned int page_count;
+	pgprot_t prot;
+	unsigned int i;
+	void *vaddr;
+
+	page_start = start - offset_in_page(start);
+	page_count = DIV_ROUND_UP(size + offset_in_page(start), PAGE_SIZE);
+	prot = pgprot_noncached(PAGE_KERNEL);
+
+	pages = kmalloc_array(page_count, sizeof(struct page *), GFP_KERNEL);
+	if (!pages) {
+		pr_err("%s: Failed to allocate array for %u pages\n",
+		       __func__, page_count);
+		return NULL;
+	}
+
+	for (i = 0; i < page_count; i++) {
+		phys_addr_t addr = page_start + i * PAGE_SIZE;
+		pages[i] = pfn_to_page(addr >> PAGE_SHIFT);
+	}
+	vaddr = vmap(pages, page_count, VM_MAP, prot);
+	kfree(pages);
+	return vaddr;
+}
+
+static struct hpsc_shmem_region *
+hpsc_msg_tp_shmem_parse_dt_mreg(struct hpsc_msg_tp_shmem_dev *tdev,
+				     const char *name, enum direction_mask dir)
 {
 	struct device_node *np;
 	struct resource res;
@@ -90,27 +127,40 @@ static int hpsc_msg_tp_shmem_parse_dt_mreg(struct hpsc_msg_tp_shmem_dev *tdev,
 	np = of_parse_phandle(tdev->dev->of_node, name, 0);
 	if (!np) {
 		dev_err(tdev->dev, "no DT '%s' property\n", name);
-		return -EFAULT;
+		return NULL;
 	}
 	ret = of_address_to_resource(np, 0, &res);
 	if (ret) {
-		dev_err(tdev->dev, "no address for DT '%s'\n", name);
-		return ret;
+		dev_err(tdev->dev, "no address for DT '%s': rc %d\n", name, ret);
+		return NULL;
 	}
 	// parse and map into kernel virtual memory
 	size = resource_size(&res);
 	if (size < sizeof(struct hpsc_shmem_region)) {
 		dev_err(tdev->dev, "size of DT '%s' is too small\n", name);
-		return -ENOMEM;
+		return NULL;
 	}
-	// use writecombine flag to prevent caching
-	vaddr = devm_memremap(tdev->dev, res.start, size, MEMREMAP_WC);
+
+	tdev->is_ram |= pfn_valid(res.start >> PAGE_SHIFT) ? dir : 0;
+	if (tdev->is_ram & dir) {
+		vaddr = hpsc_msg_tp_shmem_vmap(res.start, size);
+	} else {
+		vaddr = devm_memremap(tdev->dev, res.start, size, MEMREMAP_WT);
+	}
 	if (!vaddr) {
-		dev_err(tdev->dev, "devm_memremap failed\n");
-		return -ENOMEM;
+		dev_err(tdev->dev, "failed to %s region '%s'\n",
+				(tdev->is_ram & dir) ? "vmap" : "memremap", name);
+		return NULL;
 	}
-	*reg = (struct hpsc_shmem_region *) vaddr;
-	return 0;
+	return vaddr;
+}
+
+static void hpsc_msg_tp_shmem_unmap(struct hpsc_msg_tp_shmem_dev *tdev)
+{
+	if (tdev->in && (tdev->is_ram & IN))
+		vunmap(tdev->in);
+	if (tdev->out && (tdev->is_ram & OUT))
+		vunmap(tdev->out);
 }
 
 static int hpsc_msg_tp_shmem_parse_dt(struct hpsc_msg_tp_shmem_dev *tdev)
@@ -122,9 +172,15 @@ static int hpsc_msg_tp_shmem_parse_dt(struct hpsc_msg_tp_shmem_dev *tdev)
 		dev_err(tdev->dev, "invalid DT 'poll-interval-ms' value\n");
 		return ret;
 	}
-	// now parse memory regions
-	return hpsc_msg_tp_shmem_parse_dt_mreg(tdev, "memory-region-in", &tdev->in) ||
-	       hpsc_msg_tp_shmem_parse_dt_mreg(tdev, "memory-region-out", &tdev->out);
+	tdev->in = hpsc_msg_tp_shmem_parse_dt_mreg(tdev, "memory-region-in", IN);
+	if (!tdev->in)
+		return -ENOMEM;
+	tdev->out = hpsc_msg_tp_shmem_parse_dt_mreg(tdev, "memory-region-out", OUT);
+	if (!tdev->out) {
+		hpsc_msg_tp_shmem_unmap(tdev);
+		return -ENOMEM;
+	}
+	return 0;
 }
 
 static int hpsc_msg_tp_shmem_probe(struct platform_device *pdev)
@@ -167,6 +223,7 @@ static int hpsc_msg_tp_shmem_remove(struct platform_device *pdev)
 	dev_info(tdev->dev, "remove\n");
 	ret = kthread_stop(tdev->t);
 	hpsc_notif_unregister(&tdev->nb);
+	hpsc_msg_tp_shmem_unmap(tdev);
 	return ret;
 }
 
